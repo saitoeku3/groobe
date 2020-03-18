105ff02abeb6f9883f0b126ee9e5260c58d17926
Tadao Iseki <saitoeku3@gmail.com>
2020-03-18 12:11:02 +0900

    Add custom hook to use authenticated user

---
+++ src/hooks/useCurrentUser.tsx
@@ -0,0 +1,16 @@
+import React, { useContext, createContext, useState } from 'react'
+import { CurrentUser } from '../domains/user'
+
+const CurrentUserContext = createContext<CurrentUser | undefined>(undefined)
+
+export const CurrentUserProvider: React.FC<{ currentUser?: CurrentUser }> = ({
+  children,
+  currentUser
+}) => {
+  const [user] = useState<CurrentUser | undefined>(currentUser)
+  return <CurrentUserContext.Provider value={user}>{children}</CurrentUserContext.Provider>
+}
+
+export const useCurrentUser = () => {
+  return useContext(CurrentUserContext)
+}
