b4eaea9c8906831dde016e880c3753a802291677
Tadao Iseki <saitoeku3@gmail.com>
2020-03-22 10:38:59 +0900

    Add FIREBASE_CONFIG to environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,12 +1,12 @@
 import * as admin from 'firebase-admin'
 import { env } from '../constants/env'
 
+const { DATABSE_URL, FIREBASE_CONFIG } = env
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(
-      require('../../config/serviceAccount.json') as admin.ServiceAccount
-    ),
-    databaseURL: env.DATABSE_URL
+    credential: admin.credential.cert(JSON.parse(FIREBASE_CONFIG) as admin.ServiceAccount),
+    databaseURL: DATABSE_URL
   })
 }
 
